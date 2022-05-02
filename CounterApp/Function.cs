4c858c38007707176f1080be496dc9ad738eb90d
danieldetale <72981829+danieldetale@users.noreply.github.com>
2022-05-02 16:51:00 +0300

    first commit

---
+++ CounterApp/Function.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNet.SignalR;
+
+namespace CounterApp
+{
+    internal class Function
+    {
+        private static HttpClient httpClient = new HttpClient();
+        private static string Etag = string.Empty;
+        private static string StarCount = "0";
+
+
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+             [HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req,
+             [SignalRConnectionInfo(HubName = "serverless")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("broadcast")]
+        public static async Task Broadcast([TimerTrigger("*/2 * * * * *")] TimerInfo myTimer,
+        [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://functionapp220220419172755.azurewebsites.net/api/GetCounter");
+            request.Headers.UserAgent.ParseAdd("serverless");
+            request.Headers.Add("If-None-Match", Etag);
+            var response = await httpClient.SendAsync(request);
+            if (response.Headers.Contains("Etag"))
+            {
+                Etag = response.Headers.GetValues("Etag").First();
+            }
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var result = JsonConvert.DeserializeObject<GitResult>(await response.Content.ReadAsStringAsync());
+                StarCount = result.StarCount;
+            }
+            //var serv = "serverless";
+            //IHubContext context = GlobalHost.ConnectionManager.GetHubContext();
+            //MyHub.SendMessage("The Message!");
+
+            await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "newMessage",
+                    Arguments = new[] { $"Current counter is: {StarCount}" }
+                });
+        }
+
+        private class GitResult
+        {
+            [JsonRequired]
+            [JsonProperty("counter")]
+            public string StarCount { get; set; }
+        }
+    }
+}
---
+++ CounterApp/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Azure;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using System.Reactive.Subjects;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace CounterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        HubConnection hubConnection;
+        private static readonly HttpClient client = new HttpClient();
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            SyncCounters();
+        }
+
+        async private void SyncCounters()
+        {
+            SignalRSetup();
+            await SignalRConnect();
+        }
+
+        private void SignalRSetup()
+        {
+            hubConnection = new HubConnectionBuilder().WithUrl($"https://functionapp220220419172755.azurewebsites.net/api/").Build();
+            hubConnection.On<string>("newMessage", (message) =>
+            {
+                var receivedMessage = $"{message}";
+                label1.Text = receivedMessage;
+                //label2.Text = "Current counter 2 is: 0";
+            });
+
+        }
+
+        async Task SignalRConnect()
+        {
+            try
+            {
+                await hubConnection.StartAsync();
+            }
+            catch (Exception ex)
+            {
+
+                label2.Text = ex.ToString();
+            }
+        }
+
+        private async void Button_Clicked_1Async(object sender, EventArgs e)
+        {   
+            var response = await client.GetAsync("https://functionapp220220419172755.azurewebsites.net/api/GetCounter");
+            var content = await response.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(content);
+            var counter = data?.counter;
+            await Task.Yield();
+            int num = Convert.ToInt32(counter) + 1;
+            await client.GetAsync($"https://functionapp220220419172755.azurewebsites.net/api/newupdate?value={num.ToString()}");
+        }
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Increase(label2);
+        }
+
+        private void Increase(Label label)
+        {
+            int num = int.Parse(label.Text);
+            num++;
+            label.Text = num.ToString();
+        }
+    }
+
+}
